94bcda8cf180f1ad6c4b6721dc98fd162075979b
Dmitry Pleshkov <dpleshkov.usa@gmail.com>
2021-02-21 23:16:15 -0800

    75%

--- main.js
+++ main.js
@@ -6,14 +6,21 @@ const port = 3000;
 app.use(express.static("src"));
 app.use(express.json());
 
-app.get("/", (req, res) => {
-    console.log("request");
-    res.sendFile(path.join(__dirname, "/src/html/index.html"));
-})
+app.get("/", async(req, res) => {
+    return res.sendFile(path.join(__dirname, "/src/html/index.html"));
+});
 
-pinger.ping('pleshkov.dev', 25565, (error, result) => {
-    if (error) return console.error(error)
-    console.log(JSON.stringify(result))
+app.get("/status/:ip", async(req, res) => {
+    let ip = req.params.ip;
+    let hostname = ip.split(":")[0];
+    let port = Number(ip.split(":")[1]) || 25565;
+    try {
+        let status = await pinger.pingPromise(hostname, port);
+        console.log(JSON.stringify(status));
+        return res.json(status);
+    } catch(err) {
+        return res.json({"error":true});
+    }
 })
 
 app.listen(port, "0.0.0.0", () => {
---
+++ src/js/main.js
@@ -0,0 +1,56 @@
+const input = document.getElementById("input");
+const button = document.getElementById("getStatus");
+const modal = new bootstrap.Modal(document.getElementById('statusModal'), {});
+
+const parseDescription = function(description) {
+    let desc = "";
+    if (typeof description === "string") {
+        desc = description;
+        return desc;
+    }
+    description.extra = description.extra || [];
+    description.text = description.text || "";
+    for (let extraIndex in description.extra) {
+        desc += description.extra[extraIndex].text + "\n";
+    }
+    desc += description.text;
+    return desc;
+}
+
+input.addEventListener("keyup", async (evt) => {
+    if (evt.key === "Enter") {
+        let value = input.value;
+        input.value = "";
+        button.innerText = "Pinging...";
+        try {
+            document.getElementById("errorMsg").setAttribute("style", "display: none");
+            let status = await axios.get(`/status/${value}`);
+            if (status.data.error) {
+                document.getElementById("errorMsg").setAttribute("style", "display: inherit");
+                button.innerText = "Get Status";
+                return;
+            }
+            document.getElementById("statusIp").innerText = value;
+            document.getElementById("statusVersion").innerText = JSON.stringify(status.data.version.name);
+            document.getElementById("statusPlayers").innerText = JSON.stringify(status.data.players.online) + "/" + JSON.stringify(status.data.players.max);
+            document.getElementById("statusMOTD").innerText = parseDescription(status.data.description);
+            modal.show();
+            button.innerText = "Get Status";
+        } catch (err) {
+            console.log(err);
+            document.getElementById("errorMsg").setAttribute("style", "display: inherit");
+            button.innerText = "Get Status";
+        }
+    }
+});
+
+let exampleJSON = {
+    "version": {
+        "name": "Waterfall 1.8.x, 1.9.x, 1.10.x, 1.11.x, 1.12.x, 1.13.x, 1.14.x, 1.15.x, 1.16.x",
+        "protocol": 736
+    },
+    "players": {"max": 1337, "online": 2},
+    "description": {"extra": [{"text": "UninvitedSF Community Server"}], "text": ""},
+    "modinfo": {"type": "FML", "modList": []},
+    "ping": 13
+}
