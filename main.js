86700915026efb78c9fefef2f4a84d6e076cfa8b
Dmitry Pleshkov <dpleshkov.usa@gmail.com>
2021-02-21 23:22:27 -0800

    added dotenv

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const pinger = require("minecraft-pinger");
 const path = require("path");
+require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(express.static("src"));
